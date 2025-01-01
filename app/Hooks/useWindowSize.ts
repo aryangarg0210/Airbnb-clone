640626d7ec5e031ff187d8167c9cccf26c896740
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-01-01 13:08:06 +0530

    fix: fix loading state freeze on client fetch error fallback

---
+++ app/Hooks/useWindowSize.ts
@@ -0,0 +1,8 @@
+
+// Added for: fix: fix loading state freeze on client fetch error fallback (Change ID: 1)
+export class FixLoadingStateFreezeOnClientFetchErrorFallbackHelper_1 {
+  static handle_1(payload: any) {
+    console.log("Triggered helper: fix: fix loading state freeze on client fetch error fallback");
+    return payload;
+  }
+}
