16a3a843f0c8d6d43954817daeefdd2e4c9116f0
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-01-02 12:32:34 +0530

    feat: add get bookings by user id endpoint in controller

---
+++ backend/src/booking/booking.service.ts
@@ -0,0 +1,6 @@
+
+  // Added for: feat: add get bookings by user id endpoint in controller (Change ID: 10)
+  async addGetBookingsByUserIdEndpointInController_10(data: any): Promise<any> {
+    console.log('Executing service logic: feat: add get bookings by user id endpoint in controller - 10');
+    return { success: true, date: new Date(), ref: 10 };
+  }
