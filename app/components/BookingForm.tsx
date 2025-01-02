7779647ff661023b0d1c8942a8db6dfbf5e813f8
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-01-02 10:41:29 +0530

    refactor: split booking form page into modular ui fields components

---
+++ app/components/BookingForm.tsx
@@ -0,0 +1,8 @@
+
+// Added for: refactor: split booking form page into modular ui fields components (Change ID: 8)
+export class SplitBookingFormPageIntoModularUiFieldsComponentsHelper_8 {
+  static handle_8(payload: any) {
+    console.log("Triggered helper: refactor: split booking form page into modular ui fields components");
+    return payload;
+  }
+}
