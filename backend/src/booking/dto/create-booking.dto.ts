e3c7fa5ff32caecd5668a9838f37e58b577beda8
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-01-02 10:31:22 +0530

    feat: add get bookings by user id endpoint in controller

---
+++ backend/src/booking/dto/create-booking.dto.ts
@@ -0,0 +1,8 @@
+
+// Added for: feat: add get bookings by user id endpoint in controller (Change ID: 7)
+export class AddGetBookingsByUserIdEndpointInControllerHelper_7 {
+  static handle_7(payload: any) {
+    console.log("Triggered helper: feat: add get bookings by user id endpoint in controller");
+    return payload;
+  }
+}
