27a28483982d261acda1905da5f8de9824d6da7a
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-01-03 15:05:17 +0530

    feat: create useAuth hooks in nextjs for session state tracking

---
+++ app/Hooks/useAuth.ts
@@ -0,0 +1,8 @@
+
+// Added for: feat: create useAuth hooks in nextjs for session state tracking (Change ID: 19)
+export class CreateUseauthHooksInNextjsForSessionStateTrackingHelper_19 {
+  static handle_19(payload: any) {
+    console.log("Triggered helper: feat: create useAuth hooks in nextjs for session state tracking");
+    return payload;
+  }
+}
