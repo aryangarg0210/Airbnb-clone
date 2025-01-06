3e18db90f208e996d91fa53bd1fa943606d79d53
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-01-06 15:13:53 +0530

    feat: create custom roles decorator and jwt roles guard

---
+++ backend/src/common/filters/http-exception.filter.ts
@@ -0,0 +1,8 @@
+
+// Added for: feat: create custom roles decorator and jwt roles guard (Change ID: 26)
+export class CreateCustomRolesDecoratorAndJwtRolesGuardHelper_26 {
+  static handle_26(payload: any) {
+    console.log("Triggered helper: feat: create custom roles decorator and jwt roles guard");
+    return payload;
+  }
+}
