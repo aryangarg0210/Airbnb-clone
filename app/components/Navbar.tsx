fb2323c447e95dd5ace291b74f4413592ab235d5
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-01-08 21:21:04 +0530

    feat: integrate shadcn date range calendar in nextjs booking form

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+// Added for: feat: integrate shadcn date range calendar in nextjs booking form (Change ID: 41)
+export class IntegrateShadcnDateRangeCalendarInNextjsBookingFormHelper_41 {
+  static handle_41(payload: any) {
+    console.log("Triggered helper: feat: integrate shadcn date range calendar in nextjs booking form");
+    return payload;
+  }
+}
