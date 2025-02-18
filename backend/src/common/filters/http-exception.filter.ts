d379df0c30a650650d288de99f57aee4d13ac3a3
ARYAN-01-GARG <aryangarg30842@gmail.com>
2025-02-18 20:50:45 +0530

    refactor: standardise express response interface structure in nestjs

--- backend/src/common/filters/http-exception.filter.ts
+++ backend/src/common/filters/http-exception.filter.ts
@@ -214,3 +214,11 @@ export class StandardiseExpressResponseInterfaceStructureInNestjsHelper_225 {
     return payload;
   }
 }
+
+// Added for: refactor: standardise express response interface structure in nestjs (Change ID: 231)
+export class StandardiseExpressResponseInterfaceStructureInNestjsHelper_231 {
+  static handle_231(payload: any) {
+    console.log("Triggered helper: refactor: standardise express response interface structure in nestjs");
+    return payload;
+  }
+}
